2ddde87e07911ba0876236c268edd0a402367a13
Tom Hanson <tom.hanson@digitalunit.berlin>
2018-02-09 09:34:42 +0000

    added opening crawl to the app for lols

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { getData } from './utils/api';
+import OpeningCrawl from './components/openingCrawl';
 
 class App extends Component {
 	constructor(props) {
@@ -41,6 +42,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
+			<OpeningCrawl />
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">Welcome to React</h1>
